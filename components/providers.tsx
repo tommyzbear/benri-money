60ce3807b0f506dd9987a19b739f7b96e0f4331f
tommyzbear <ajdsenwill@hotmail.com>
2025-02-03 02:53:30 +0000

    Set default chain to devnet

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { PrivyProvider } from '@privy-io/react-auth';
+import { arbitrumGoerli, arbitrumSepolia, baseSepolia, optimismSepolia, sepolia } from 'viem/chains';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <PrivyProvider
             appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
             config={{
+                defaultChain: baseSepolia,
+                supportedChains: [baseSepolia, arbitrumSepolia, arbitrumGoerli, sepolia, optimismSepolia],
                 // Customize Privy's appearance in your app
                 appearance: {
                     theme: 'light',
